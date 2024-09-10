f71d80327c8fc6c561ee07cab5d0a58ecbb0f13e
Your Name <guptamadhav1729@gmail.com>
2024-09-11 02:51:18 +0530

    debug fixes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -73,7 +73,6 @@ export const login = async (req, res) => {
         httpOnly: true,
         maxAge: age,
         secure: true,
-        SameSite: "Lax",
       })
       .status(200)
       .json(loggedInUser);
--- api/middlewares/verifyToken.middleware.js
+++ api/middlewares/verifyToken.middleware.js
@@ -1,31 +1,31 @@
 import jwt from "jsonwebtoken";
 
 // type-1
-// export const verifyToken = (req, res, next) => {
-//   try {
-//     const token = req.cookies?.token;
-//     if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+export const verifyToken = (req, res, next) => {
+  try {
+    const token = req.cookies?.token;
+    if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-//     const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-//     // console.log("decodedToken:: ", decodedToken);
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    // console.log("decodedToken:: ", decodedToken);
 
-//     req.userId = decodedToken.id;
-//     next();
-//   } catch (error) {
-//     return res.status(403).json({ message: "Token is not Valid!" });
-//   }
-// };
+    req.userId = decodedToken.id;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Token is not Valid!" });
+  }
+};
 
 // type-2
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token;
+// export const verifyToken = (req, res, next) => {
+//   const token = req.cookies.token;
 
-  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+//   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid!" });
-    req.userId = payload.id;
-    console.log("req.userId::", req.userId);
-    next();
-  });
-};
+//   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+//     if (err) return res.status(403).json({ message: "Token is not Valid!" });
+//     req.userId = payload.id;
+//     console.log("req.userId::", req.userId);
+//     next();
+//   });
+// };
