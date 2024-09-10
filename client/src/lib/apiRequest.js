a70f673a67f22a4a5773687f938795655726069b
shivaay2210 <164501782+shivaay2210@users.noreply.github.com>
2024-09-10 18:04:11 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://estate-backend2.onrender.com/api",
     withCredentials: true
 })
 
